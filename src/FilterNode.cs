076933cc56638a27e2ea8c8502739a6a222db521
Andrew Harward <andrew.harward@quantium.com.au>
2019-08-02 15:59:24 +1000

    feat: Rename 'Empty' to 'True' and add 'False'

--- src/FilterNode.cs
+++ src/FilterNode.cs
@@ -6,7 +6,15 @@ namespace ExtremeAndy.CombinatoryFilters
     public abstract class FilterNode<TLeafNode> : FilterNode, IFilterNode<TLeafNode>
         where TLeafNode : class, ILeafFilterNode
     {
-        public static OrderedCombinationFilter<TLeafNode> Empty = new OrderedCombinationFilter<TLeafNode>(new IFilterNode<TLeafNode>[0], CombinationOperator.All);
+        /// <summary>
+        /// Empty filter which is the equivalent of 'true'
+        /// </summary>
+        public static OrderedCombinationFilter<TLeafNode> True = new OrderedCombinationFilter<TLeafNode>(new IFilterNode<TLeafNode>[0], CombinationOperator.All);
+
+        /// <summary>
+        /// Empty filter which is the equivalent of 'false'
+        /// </summary>
+        public static OrderedCombinationFilter<TLeafNode> False = new OrderedCombinationFilter<TLeafNode>(new IFilterNode<TLeafNode>[0], CombinationOperator.Any);
 
         public abstract TResult Aggregate<TResult>(
             Func<IEnumerable<TResult>, CombinationOperator, TResult> combine,
