68f31934e6b1f4c652bc31914b7fb32b988074d5
Andrew Harward <andrew.harward@quantium.com.au>
2020-10-21 10:47:31 +1100

    docs: Added some comments against IFilterNode.IsTrue and IFilterNode.IsFalse

--- src/IFilterNode.cs
+++ src/IFilterNode.cs
@@ -30,8 +30,30 @@ namespace ExtremeAndy.CombinatoryFilters
 
         bool Any(Func<TLeafNode, bool> predicate);
 
+        /// <summary>
+        /// Returns <see langword="true" /> if the filter is will always evaluate to <see langword="true" />
+        /// regardless of the input, i.e. the filter is trivial and will include everything.
+        ///
+        /// Returns <see langword="false" /> if it is not known whether the filter will evaluate to <see langword="true" />
+        /// until an input is tested.
+        ///
+        /// Note that <see cref="IsTrue"/> returning <see langword="false" /> does *not* automatically imply that
+        /// <see cref="IsFalse"/> will return <see langword="true" />.
+        /// </summary>
+        /// <returns></returns>
         bool IsTrue();
 
+        /// <summary>
+        /// Returns <see langword="true" /> if the filter is will always evaluate to <see langword="false" />
+        /// regardless of the input, i.e. the filter is trivial and will include nothing.
+        ///
+        /// Returns <see langword="false" /> if it is not known whether the filter will evaluate to <see langword="false" />
+        /// until an input is tested.
+        ///
+        /// Note that <see cref="IsFalse"/> returning <see langword="false" /> does *not* automatically imply that
+        /// <see cref="IsTrue"/> will return <see langword="true" />.
+        /// </summary>
+        /// <returns></returns>
         bool IsFalse();
     }
 }
