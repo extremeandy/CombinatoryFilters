08b0e3b6518eab904ca52390f0ccdcc79ffc23ba
Andrew Harward <andrew.harward@quantium.com.au>
2021-02-25 09:35:49 +1100

    feat: Added method FilterNode.IsEquivalent which tests for logical equivalence between filters

---
+++ src/FilterNode.IsEquivalent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtremeAndy.CombinatoryFilters
+{
+    public static partial class FilterNodeExtensions
+    {
+        public static bool IsEquivalent<TLeafNode>(
+            this IFilterNode<TLeafNode> filter,
+            IFilterNode<TLeafNode> other)
+            where TLeafNode : class, ILeafFilterNode
+            => filter.Collapse().Equals(other.Collapse());
+
+    }
+}
